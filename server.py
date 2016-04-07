b725297e156d756086a6a7933e0529fddcc7ee5f
Alan Berman <brmala003@.cs.uct.ac.za>
2016-04-07 16:21:17 +0200

    initialize

---
+++ server.py
@@ -0,0 +1,21 @@
+# all the imports
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, \
+     abort, render_template, flash
+
+app = Flask(__name__)
+
+@app.route("/home")
+def home():
+	return render_template("home.html")
+	
+@app.route("/about")
+def about():
+	return render_template("about.html")
+	
+@app.route("/contact")
+def contact():
+	return render_template("contact.html")
+
+if __name__ == '__main__':
+    app.run()
